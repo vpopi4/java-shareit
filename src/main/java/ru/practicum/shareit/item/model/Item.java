7c3c5cebeaeaea5ac7dc43204d364546436849b9
vpopi4 <vpopi4@yandex.ru>
2024-09-27 02:55:09 +0300

    feat: add model classes

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,28 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import org.apache.catalina.connector.Request;
+import ru.practicum.shareit.user.User;
+
+@Data
+@Builder
 public class Item {
+    @NotNull
+    private Integer id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private Boolean isAvailable;
+
+    @NotNull
+    private User owner;
+
+    private Request request;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class User {
+    @NotNull
+    private Integer id;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String name;
 }

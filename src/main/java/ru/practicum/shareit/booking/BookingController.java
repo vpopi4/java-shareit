90ef607ea01b5e86617fe12d1e169a9b45db6fe8
vpopi4 <vpopi4@yandex.ru>
2024-12-25 04:10:33 +0300

    feat: add booking controller and service interface

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,60 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingCreatingDto;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingState;
+import ru.practicum.shareit.util.ClientException;
 
-/**
- * TODO Sprint add-bookings.
- */
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/bookings")
+@RequiredArgsConstructor
+@Slf4j
 public class BookingController {
+    private final BookingService service;
+
+    @PostMapping
+    public BookingDto createItem(
+            @RequestHeader("X-Sharer-User-Id") Integer userId,
+            @Valid @RequestBody BookingCreatingDto dto
+    ) throws ClientException {
+        return service.createItem(userId, dto);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public BookingDto approveOrRejectBooking(
+            @RequestHeader("X-Sharer-User-Id") Integer userId,
+            @PathVariable("bookingId") Integer bookingId,
+            @RequestParam("approved") Boolean isApproved
+    ) throws ClientException {
+        return service.approveOrRejectBooking(userId, bookingId, isApproved);
+    }
+
+    @GetMapping("/{bookingId}")
+    public BookingDto getBooking(
+            @RequestHeader("X-Sharer-User-Id") Integer userId,
+            @PathVariable("bookingId") Integer bookingId) throws ClientException {
+        return service.getBooking(userId, bookingId);
+    }
+
+    @GetMapping
+    public List<BookingDto> getBookingsByBooker(
+            @RequestHeader("X-Sharer-User-Id") Integer userId,
+            @RequestParam("state") String state
+    ) throws ClientException {
+        return service.getBookingsByBooker(userId, BookingState.parseString(state));
+    }
+
+    @GetMapping("/owner")
+    public List<BookingDto> getBookingsByOwner(
+            @RequestHeader("X-Sharer-User-Id") Integer userId,
+            @RequestParam("state") String state
+    ) throws ClientException {
+        return service.getBookingsByOwner(userId, BookingState.parseString(state));
+    }
 }
---
+++ src/main/java/ru/practicum/shareit/booking/BookingService.java
@@ -0,0 +1,27 @@
+package ru.practicum.shareit.booking;
+
+import ru.practicum.shareit.booking.dto.BookingCreatingDto;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingState;
+import ru.practicum.shareit.util.ClientException;
+
+import java.util.List;
+
+public interface BookingService {
+
+    BookingDto createItem(Integer userId,
+                          BookingCreatingDto dto) throws ClientException;
+
+    BookingDto approveOrRejectBooking(Integer userId,
+                                      Integer bookingId,
+                                      Boolean isApproved) throws ClientException;
+
+    BookingDto getBooking(Integer userId,
+                          Integer bookingId) throws ClientException;
+
+    List<BookingDto> getBookingsByBooker(Integer userId,
+                                         BookingState state) throws ClientException;
+
+    List<BookingDto> getBookingsByOwner(Integer userId,
+                                        BookingState state) throws ClientException;
+}

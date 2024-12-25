a381430dd9cf5b29b8d9634b4f06bb237cd7e47c
vpopi4 <vpopi4@yandex.ru>
2024-12-25 07:05:02 +0300

    fix: change incorrect psql queries

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -11,33 +11,33 @@ import java.util.List;
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
     @Query("SELECT b FROM Booking b " +
             "WHERE b.booker.id = :bookerId " +
-            "ORDER BY b.startDate DESC")
+            "ORDER BY b.start DESC")
     List<Booking> findAllBookingsByBooker(@Param("bookerId") Integer bookerId);
 
     @Query("SELECT b FROM Booking b " +
             "WHERE b.booker.id = :bookerId " +
-            "AND b.startDate <= CURRENT_TIMESTAMP " +
-            "AND b.endDate >= CURRENT_TIMESTAMP " +
+            "AND b.start <= CURRENT_TIMESTAMP " +
+            "AND b.end >= CURRENT_TIMESTAMP " +
             "AND b.status = 'APPROVED' " +
-            "ORDER BY b.startDate DESC")
+            "ORDER BY b.start DESC")
     List<Booking> findCurrentBookingsByBooker(@Param("bookerId") Integer bookerId);
 
     @Query("SELECT b FROM Booking b " +
             "WHERE b.booker.id = :bookerId " +
-            "AND b.endDate < CURRENT_TIMESTAMP " +
-            "ORDER BY b.startDate DESC")
+            "AND b.end < CURRENT_TIMESTAMP " +
+            "ORDER BY b.start DESC")
     List<Booking> findPastBookingsByBooker(@Param("bookerId") Integer bookerId);
 
     @Query("SELECT b FROM Booking b " +
             "WHERE b.booker.id = :bookerId " +
-            "AND b.startDate > CURRENT_TIMESTAMP " +
-            "ORDER BY b.startDate DESC")
+            "AND b.start > CURRENT_TIMESTAMP " +
+            "ORDER BY b.start DESC")
     List<Booking> findFutureBookingsByBooker(@Param("bookerId") Integer bookerId);
 
     @Query("SELECT b FROM Booking b " +
             "WHERE b.booker.id = :bookerId " +
             "AND b.status = :status " +
-            "ORDER BY b.startDate DESC")
+            "ORDER BY b.start DESC")
     List<Booking> findByStatusAndBooker(@Param("status") BookingStatus status,
                                         @Param("bookerId") Integer bookerId);
 
@@ -45,33 +45,33 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
             "JOIN b.item i " +
             "JOIN i.owner o " +
             "WHERE o.id = :ownerId " +
-            "ORDER BY b.startDate DESC")
+            "ORDER BY b.start DESC")
     List<Booking> findAllBookingsByOwner(@Param("ownerId") Integer ownerId);
 
     @Query("SELECT b FROM Booking b " +
             "JOIN b.item i " +
             "JOIN i.owner o " +
             "WHERE o.id = :ownerId " +
-            "AND b.startDate <= CURRENT_TIMESTAMP " +
-            "AND b.endDate >= CURRENT_TIMESTAMP " +
+            "AND b.start <= CURRENT_TIMESTAMP " +
+            "AND b.end >= CURRENT_TIMESTAMP " +
             "AND b.status = 'APPROVED' " +
-            "ORDER BY b.startDate DESC")
+            "ORDER BY b.start DESC")
     List<Booking> findCurrentBookingsByOwner(@Param("ownerId") Integer ownerId);
 
     @Query("SELECT b FROM Booking b " +
             "JOIN b.item i " +
             "JOIN i.owner o " +
             "WHERE o.id = :ownerId " +
-            "AND b.endDate < CURRENT_TIMESTAMP " +
-            "ORDER BY b.startDate DESC")
+            "AND b.end < CURRENT_TIMESTAMP " +
+            "ORDER BY b.start DESC")
     List<Booking> findPastBookingsByOwner(@Param("ownerId") Integer ownerId);
 
     @Query("SELECT b FROM Booking b " +
             "JOIN b.item i " +
             "JOIN i.owner o " +
             "WHERE o.id = :ownerId " +
-            "AND b.startDate > CURRENT_TIMESTAMP " +
-            "ORDER BY b.startDate DESC")
+            "AND b.start > CURRENT_TIMESTAMP " +
+            "ORDER BY b.start DESC")
     List<Booking> findFutureBookingsByOwner(@Param("ownerId") Integer ownerId);
 
     @Query("SELECT b FROM Booking b " +
@@ -79,7 +79,7 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
             "JOIN i.owner o " +
             "WHERE o.id = :ownerId " +
             "AND b.status = :status " +
-            "ORDER BY b.startDate DESC")
+            "ORDER BY b.start DESC")
     List<Booking> findByStatusAndOwner(@Param("status") BookingStatus status,
                                        @Param("ownerId") Integer ownerId);
 

1ddc3cfab245943be9c7d095a910baaa9aa42b47
vpopi4 <vpopi4@yandex.ru>
2025-01-09 11:41:09 +0300

    feat (request): implement service

---
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
@@ -0,0 +1,105 @@
+package ru.practicum.shareit.request;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.request.dto.ItemRequestCreationDto;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.ItemRequestShortDto;
+import ru.practicum.shareit.request.dto.ItemShortDto;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.UserRepository;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.util.ClientException;
+import ru.practicum.shareit.util.NotFoundException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ItemRequestServiceImpl implements ItemRequestService {
+    private final ItemRequestRepository repository;
+    private final UserRepository userRepository;
+
+    @Override
+    public ItemRequestShortDto createItemRequest(Integer userId,
+                                                 ItemRequestCreationDto dto) throws ClientException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("user not found"));
+
+        ItemRequest.ItemRequestBuilder itemRequestBuilder = ItemRequest.builder()
+                .id(null)
+                .description(dto.getDescription())
+                .requester(user)
+                .createdAt(LocalDateTime.now());
+
+        ItemRequest request = repository.save(itemRequestBuilder.build());
+
+        return ItemRequestShortDto.builder()
+                .id(request.getId())
+                .description(request.getDescription())
+                .created(request.getCreatedAt())
+                .build();
+    }
+
+    @Override
+    public List<ItemRequestDto> findAllByUserId(Integer userId) throws ClientException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("user not found"));
+
+        return repository
+                .findAllByRequesterId(userId)
+                .stream()
+                .map((request) -> ItemRequestDto.builder()
+                        .id(request.getId())
+                        .description(request.getDescription())
+                        .created(request.getCreatedAt())
+                        .items(request.getItems()
+                                .stream()
+                                .map((item) -> ItemShortDto.builder()
+                                        .id(item.getId())
+                                        .name(item.getName())
+                                        .ownerId(item.getOwner().getId())
+                                        .build())
+                                .toList())
+                        .build())
+                .toList();
+    }
+
+    @Override
+    public List<ItemRequestShortDto> findAll() throws ClientException {
+        return repository
+                .findAllOrderByCreatedAtDesc()
+                .stream()
+                .map((request) -> ItemRequestShortDto.builder()
+                        .id(request.getId())
+                        .description(request.getDescription())
+                        .created(request.getCreatedAt())
+                        .build())
+                .toList();
+    }
+
+    @Override
+    public ItemRequestDto findById(Integer requestId) throws ClientException {
+        ItemRequest request = repository.findByIdWithItems(requestId)
+                .orElseThrow(() -> new NotFoundException("request not found"));
+
+        log.info("item request = {}", request);
+
+        return ItemRequestDto.builder()
+                .id(request.getId())
+                .description(request.getDescription())
+                .created(request.getCreatedAt())
+                .items(request.getItems()
+                        .stream()
+                        .map((item) -> ItemShortDto.builder()
+                                .id(item.getId())
+                                .name(item.getName())
+                                .ownerId(item.getOwner().getId())
+                                .build())
+                        .toList())
+                .build();
+    }
+}

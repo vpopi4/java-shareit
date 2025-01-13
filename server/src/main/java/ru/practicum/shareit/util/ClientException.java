1459eb6113bf33f6a4ca44df9e3d1af844a330a1
vpopi4 <vpopi4@yandex.ru>
2025-01-13 17:40:56 +0300

    refactor: remove unused constructors from ClientException class

--- server/src/main/java/ru/practicum/shareit/util/ClientException.java
+++ server/src/main/java/ru/practicum/shareit/util/ClientException.java
@@ -3,19 +3,8 @@ package ru.practicum.shareit.util;
 import org.springframework.http.HttpStatus;
 
 public abstract class ClientException extends Exception {
-    public ClientException() {
-    }
-
     public ClientException(String message) {
         super(message);
-    }
-
-    public ClientException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ClientException(Throwable cause) {
-        super(cause);
     }
 
     public ClientException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {

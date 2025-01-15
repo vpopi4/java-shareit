e45071262e3a329504ad185086d96b907732748d
vpopi4 <vpopi4@yandex.ru>
2025-01-15 03:57:29 +0300

    feat: add validation for CommentCreationDto and ItemRequestCreationDto fields

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentCreationDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentCreationDto.java
@@ -2,12 +2,14 @@ package ru.practicum.shareit.item.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
 public class CommentCreationDto {
+    @NotBlank
     String text;
 
     @JsonCreator
